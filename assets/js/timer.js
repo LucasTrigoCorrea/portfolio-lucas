0e9de40703a086d41416c149d56b29efd4e37578
Lucas Trigo Correa <lucastrigocorrea@gmail.com>
2023-08-22 21:28:06 -0300

    changed timer

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,43 +1,52 @@
-function relogio() {
-  function criaHoraDosSegundos(segundos) {
-    const data = new Date(segundos * 1000);
-    return data.toLocaleTimeString('pt-BR', {
-      hour12: false,
-      timeZone: 'UTC'
+//SCRIPT JS FOR TIMER
+function timer() {
+//FUNCTION CONFIGURE SECONDS
+function getTimeFromSeconds(seconds) {
+    const date = new Date(seconds * 1000);
+    return date.toLocaleTimeString('pt-BR', { 
+        Hour12: false, 
+        timeZone: 'UTC',
     });
-  }
-
-  const relogio = document.querySelector('.relogio');
-  let segundos = 0;
-  let timer;
-
-  function iniciaRelogio() {
-    timer = setInterval(function() {
-      segundos++;
-      relogio.innerHTML = criaHoraDosSegundos(segundos);
-    }, 1000);
-  }
-
-  document.addEventListener('click', function(e) {
-    const el = e.target;
-
-    if (el.classList.contains('zerar')) {
-      clearInterval(timer);
-      relogio.innerHTML = '00:00:00';
-      relogio.classList.remove('pausado');
-      segundos = 0;
-    }
-
-    if (el.classList.contains('iniciar')) {
-      relogio.classList.remove('pausado');
-      clearInterval(timer);
-      iniciaRelogio();
-    }
-
-    if (el.classList.contains('pausar')) {
-      clearInterval(timer);
-      relogio.classList.add('pausado');
-    }
-  });
 }
-relogio();
+
+const timer = document.querySelector('.timer');
+let seconds = 0;
+let timerOn;
+
+//FUNCTION COUNTING TIMER
+function initTimer() {
+    timerOn = setInterval(function() {
+        seconds++;
+        timer.innerHTML = getTimeFromSeconds(seconds);
+    }, 1000 );
+}
+
+//ELEMENT CLICKED
+document.addEventListener('click', function(e) {
+    const element = e.target;
+//BUTTON RESET
+    if(element.classList.contains('reset')) {
+        timer.classList.remove('paused');
+        clearInterval(timerOn);
+        timer.innerHTML = '00:00:00';
+        seconds = 0;
+    }
+//BUTTON PAUSE
+    if(element.classList.contains('pause')) {
+        timer.classList.add('paused');
+        clearInterval(timerOn);
+    }
+//BUTTON INIT
+    if(element.classList.contains('init')) {
+        timer.classList.remove('paused');
+        clearInterval(timerOn);
+        initTimer();
+    }
+});
+}
+timer()
+
+
+
+
+
